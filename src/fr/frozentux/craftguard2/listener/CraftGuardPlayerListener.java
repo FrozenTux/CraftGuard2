c0d64d4724c1bd04abbb75b08a814e5bc82b19e3
FrozenTux <valentinus1@gmail.com>
2012-09-29 22:56:46 +0200

    Beginings of the listener and the manager

---
+++ src/fr/frozentux/craftguard2/listener/CraftGuardPlayerListener.java
@@ -0,0 +1,19 @@
+package fr.frozentux.craftguard2.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+/**
+ * Listener for all players-related actions including login and inventory click
+ * @author FrozenTux
+ *
+ */
+public class CraftGuardPlayerListener implements Listener {
+	
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent e){
+		
+	}
+	
+}
