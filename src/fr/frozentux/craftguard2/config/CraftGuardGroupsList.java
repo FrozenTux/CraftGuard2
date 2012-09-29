c015e8114eb75bf4c859b33c1b3b817a096846d2
FrozenTux <valentinus1@gmail.com>
2012-09-30 00:33:58 +0200

    Added CraftGuardGroupList

---
+++ src/fr/frozentux/craftguard2/config/CraftGuardGroupsList.java
@@ -0,0 +1,32 @@
+package fr.frozentux.craftguard2.config;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
+
+/**
+ * Loads, writes and stores configuration options for CraftGuard
+ * @author FrozenTux
+ *
+ */
+public class CraftGuardGroupsList {
+	
+	private Plugin plugin;
+	
+	private HashMap<String, HashMap<Integer, ArrayList<Integer>>> groupsLists;
+	
+	private File configurationFile;
+	private FileConfiguration configuration;
+	
+	public CraftGuardGroupsList(Plugin plugin, FileConfiguration fileConfiguration, File file){
+		this.plugin = plugin;
+		this.configurationFile = file;
+		this.configuration = fileConfiguration;
+	}
+	
+	
+	
+}
