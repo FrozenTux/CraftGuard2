2f29ffb54022e8ba7e008de9a7f51733be4fb5c5
FrozenTux <valentinus1@gmail.com>
2012-10-10 15:56:33 +0200

    Added the smeltreference

---
+++ src/fr/frozentux/craftguard2/config/SmeltReference.java
@@ -0,0 +1,35 @@
+package fr.frozentux.craftguard2.config;
+
+import java.util.HashMap;
+
+/**
+ * Class storing what an object gives when smelted
+ * @author FrozenTux
+ *
+ */
+public class SmeltReference {
+	
+	private static HashMap<Integer, Integer> reference = new HashMap<Integer, Integer>();
+	
+	private static SmeltReference singleton = new SmeltReference();
+	
+	public SmeltReference getReference(){
+		return singleton;
+	}
+	
+	/**
+	 * Get what the following id will give when smelted
+	 * @param id	The id to check
+	 * @return		The smelting value or the id itself if not registered
+	 */
+	public int getSmelting(int id){
+		Integer result = reference.get(id);
+		if(result != null)return result;
+		else return id;
+	}
+	
+	public void addSmelting(int id, int result){
+		reference.put(id, result);
+	}
+
+}
