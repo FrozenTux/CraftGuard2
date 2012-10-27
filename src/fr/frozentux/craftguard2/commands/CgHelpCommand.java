37241d9f33cf8e14d5c48196984c809b0f7a16eb
FrozenTux <valentinus1@gmail.com>
2012-10-27 15:53:16 +0200

    Added addlist and removelist to help command

--- src/fr/frozentux/craftguard2/commands/CgHelpCommand.java
+++ src/fr/frozentux/craftguard2/commands/CgHelpCommand.java
@@ -19,6 +19,8 @@ public class CgHelpCommand extends CgCommandComponent {
 		sender.sendMessage(ChatColor.GREEN + "/cg remove <list> <id> " + ChatColor.AQUA + " Removes <id> from the specified <list> (does not support metadata !)");
 		sender.sendMessage(ChatColor.GREEN + "/cg list <list> " + ChatColor.AQUA + " Lists the content of the specified <list>");
 		sender.sendMessage(ChatColor.GREEN + "/cg reload " + ChatColor.AQUA + " Reloads configuration, lists and smeltings from files");
+		sender.sendMessage(ChatColor.GREEN + "/cg addlist <list> " + ChatColor.AQUA + " Adds a new empty list with the specified name");
+		sender.sendMessage(ChatColor.GREEN + "/cg removelist <list> " + ChatColor.AQUA + " Deletes the specified <list>");
 
 		return false;
 	}
