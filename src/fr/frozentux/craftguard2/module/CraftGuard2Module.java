71cd4a94ed6b03e6cb18fabe1c2180840ab78f51
FrozenTux <valentinus1@gmail.com>
2012-11-14 21:37:07 +0100

    Added the new module

---
+++ src/fr/frozentux/craftguard2/module/CraftGuard2Module.java
@@ -0,0 +1,44 @@
+package fr.frozentux.craftguard2.module;
+
+import java.io.File;
+
+import org.bukkit.event.Listener;
+
+import fr.frozentux.craftguard2.CraftGuardPlugin;
+import fr.frozentux.craftguard2.list.TypeListLoader;
+
+/**
+ * This class represents a function of CrafGuard2 (for instance craft, use) and everything needed to make it work
+ * @author FrozenTux
+ *
+ */
+public class CraftGuard2Module {
+
+	protected Listener listener;
+	
+	protected TypeListLoader loader;
+	
+	protected String type;
+	
+	protected CraftGuardPlugin plugin;
+	
+	public CraftGuard2Module(String type, Listener listener, CraftGuardPlugin plugin){
+		this.type = type.toLowerCase();
+		this.listener = listener;
+		File file = new File("plugins" + File.separator + "CraftGuard" + File.separator + type + ".yml");
+		this.loader = new TypeListLoader(type, file, this, "CraftGuard " + type + " file\nBy FrozenTux");
+	}
+	
+	public String getType(){
+		return type;
+	}
+	
+	public Listener getListener(){
+		return listener;
+	}
+	
+	public TypeListLoader getLoader(){
+		return loader;
+	}
+	
+}
