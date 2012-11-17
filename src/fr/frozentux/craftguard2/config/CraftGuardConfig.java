e9c4231401d292f9c9581ced662397bf3a8e07d0
FrozenTux <valentinus1@gmail.com>
2012-11-17 22:44:24 +0100

    Added a module list default value to the config

--- src/fr/frozentux/craftguard2/config/CraftGuardConfig.java
+++ src/fr/frozentux/craftguard2/config/CraftGuardConfig.java
@@ -27,8 +27,10 @@ public class CraftGuardConfig {
 	
 	private HashMap<String, Object> fields;
 	
-	private String[] defaultKeys = {"checkfurnaces", "preventiveallow", "denymessage", "log", "logmessage", "baseperm", "debug", "notifyplayer"};
-	private Object[] defaultValues = {true, true, "You can't craft %n !", true, "%p tried to craft %n(%i) but has been denied", "craftguard", false, true};
+	private static List<String> modules = Arrays.asList("craft", "use");
+	
+	private String[] defaultKeys = {"checkfurnaces", "preventiveallow", "denymessage", "log", "logmessage", "baseperm", "debug", "notifyplayer", "modules"};
+	private Object[] defaultValues = {true, true, "You can't craft %n !", true, "%p tried to craft %n(%i) but has been denied", "craftguard", false, true, modules};
 	
 	/**
 	 * Loads, writes and stores configuration options for CraftGuard
