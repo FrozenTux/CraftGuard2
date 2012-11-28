2098c5ceef4a5f57a542ba4bb158e251f9c5d804
FrozenTux <valentinus1@gmail.com>
2012-11-28 18:07:21 +0100

    Removed TypeListLoader from the module

--- src/fr/frozentux/craftguard2/module/CraftGuardModule.java
+++ src/fr/frozentux/craftguard2/module/CraftGuardModule.java
@@ -1,11 +1,8 @@
 package fr.frozentux.craftguard2.module;
 
-import java.io.File;
-
 import org.bukkit.event.Listener;
 
 import fr.frozentux.craftguard2.CraftGuardPlugin;
-import fr.frozentux.craftguard2.list.TypeListLoader;
 
 /**
  * This class represents a function of CrafGuard2 (for instance craft, use) and everything needed to make it work
@@ -16,8 +13,6 @@ public class CraftGuardModule {
 
 	protected Listener listener;
 	
-	protected TypeListLoader loader;
-	
 	protected String type;
 	
 	protected CraftGuardPlugin plugin;
@@ -25,8 +20,6 @@ public class CraftGuardModule {
 	public CraftGuardModule(String type, Listener listener, CraftGuardPlugin plugin){
 		this.type = type.toLowerCase();
 		this.listener = listener;
-		File file = new File("plugins" + File.separator + "CraftGuard" + File.separator + type + ".yml");
-		this.loader = new TypeListLoader(type, file, plugin.getListManager(), "CraftGuard " + type + " file\nBy FrozenTux");
 	}
 	
 	public String getType(){
@@ -35,10 +28,6 @@ public class CraftGuardModule {
 	
 	public Listener getListener(){
 		return listener;
-	}
-	
-	public TypeListLoader getLoader(){
-		return loader;
 	}
 	
 	public CraftGuardPlugin getCraftguard(){
