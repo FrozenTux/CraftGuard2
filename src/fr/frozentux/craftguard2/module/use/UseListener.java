5c76e2f948625233b2766cbf2a3add34246ef38c
FrozenTux <valentinus1@gmail.com>
2013-01-01 21:50:20 +0100

    Added a check to avoid use module blocking if the block or the entity interacted is opening a gui/performing an action

--- src/fr/frozentux/craftguard2/module/use/UseListener.java
+++ src/fr/frozentux/craftguard2/module/use/UseListener.java
@@ -1,6 +1,10 @@
 package fr.frozentux.craftguard2.module.use;
 
+import java.util.ArrayList;
+
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,6 +21,34 @@ public class UseListener implements Listener {
 
 	private CraftGuardPlugin plugin;
 	
+	private static ArrayList<Material> specialBlocks = new ArrayList<Material>();
+	
+	static{
+		
+		specialBlocks.add(Material.DISPENSER);
+		specialBlocks.add(Material.NOTE_BLOCK);
+		specialBlocks.add(Material.BED_BLOCK);
+		specialBlocks.add(Material.CHEST);
+		specialBlocks.add(Material.WORKBENCH);
+		specialBlocks.add(Material.FURNACE);
+		specialBlocks.add(Material.BURNING_FURNACE);
+		specialBlocks.add(Material.SIGN_POST);
+		specialBlocks.add(Material.WOODEN_DOOR);
+		specialBlocks.add(Material.WALL_SIGN);
+		specialBlocks.add(Material.LEVER);
+		specialBlocks.add(Material.STONE_BUTTON);
+		specialBlocks.add(Material.JUKEBOX);
+		specialBlocks.add(Material.DIODE_BLOCK_OFF);
+		specialBlocks.add(Material.DIODE_BLOCK_ON);
+		specialBlocks.add(Material.TRAP_DOOR);
+		specialBlocks.add(Material.FENCE_GATE);
+		specialBlocks.add(Material.ENCHANTMENT_TABLE);
+		specialBlocks.add(Material.BREWING_STAND);
+		specialBlocks.add(Material.COMMAND);
+		specialBlocks.add(Material.BEACON);
+		specialBlocks.add(Material.ANVIL);
+	}
+	
 	public UseListener(CraftGuardPlugin plugin){
 		this.plugin = plugin;
 	}
@@ -27,7 +59,8 @@ public class UseListener implements Listener {
 			Player p = e.getPlayer();
 			ItemStack i = e.getItem();
 			boolean denied = false;
-			if(i != null && i.getType() != Material.AIR)denied = !PermissionChecker.check(p, i.getTypeId(), i.getData().getData(), plugin, "use");
+			boolean special = (e.getClickedBlock() == null) ? false : specialBlocks.contains(e.getClickedBlock().getType());
+			if(i != null && i.getType() != Material.AIR && !special)denied = !PermissionChecker.check(p, i.getTypeId(), i.getData().getData(), plugin, "use");
 			if(denied){
 				e.setCancelled(denied);
 				p.updateInventory();
@@ -40,7 +73,7 @@ public class UseListener implements Listener {
 		Player p = e.getPlayer();
 		ItemStack i = p.getItemInHand();
 		boolean denied = false;
-		if(i != null && i.getType() != Material.AIR)denied = !PermissionChecker.check(p, i.getTypeId(), i.getData().getData(), plugin, "use");
+		if(i != null && i.getType() != Material.AIR && !e.getRightClicked().getType().equals(EntityType.VILLAGER))denied = !PermissionChecker.check(p, i.getTypeId(), i.getData().getData(), plugin, "use");
 		if(denied){
 			e.setCancelled(denied);
 			p.updateInventory();
